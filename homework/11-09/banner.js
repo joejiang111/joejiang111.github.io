3dd824b150a4b50d9d1dbf6a5c0876e267bf8fb9
joejiang111 <jyhjoe@gmail.com>
2021-11-15 17:38:16 -0500

    l

---
+++ homework/11-09/banner.js
@@ -0,0 +1,27 @@
+var url = "https://api.thecatapi.com/v1/images/search";
+d3.json(url, function(error, data) {
+
+  if (!error) {
+    d3.select("#banner")
+      .style("background-image", "url('" + data[0].url + "')");
+  }
+
+});
+
+    d3.select(window)
+        .on("scroll", function(){
+            console.log(window.scrollY);
+        })
+
+        var bannerPosition = d3.scaleLinear()
+        .domain([0, window.innerHeight])
+        .range([100, 0]);
+        
+    d3.select(window)
+        .on("scroll", function() {
+  
+        var y = bannerPosition(window.scrollY);
+        d3.select("#banner")
+            .style("background-position", "50% " + y + "%");
+    
+    });
---
+++ homework/11-09/chart1.js
@@ -0,0 +1,16 @@
+d3.select("#chart1") 
+    .on("mousemove", function(){
+        console.log(d3.event);
+    
+
+    var tooltip = d3.select("#tooltip")
+        .style("display", "block")
+        .style("top", d3.event.pageY + 20 + "px")
+        .style("left", d3.event.pageX + 20 + "px")
+        .html("Welcome to the first chart!");
+    })
+    .on("mouseout", function(){
+        d3.select("#tooltip")
+            .style("display", "none");
+        
+    });
