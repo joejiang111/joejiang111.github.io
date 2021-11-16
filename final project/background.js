bd68a7676341c772ca4b0225e9d12e2552743003
joejiang111 <jyhjoe@gmail.com>
2021-11-15 21:41:11 -0500

    1

---
+++ final project/background.js
@@ -0,0 +1,2 @@
+var url = "https://www.teahub.io/photos/full/111-1119967_cyberpunk-2077-yellow-plain-background-wallpaper-cyberpunk-2077.jpg"
+d3.select("#banner").style("background-image", "url('" + url + "')");
