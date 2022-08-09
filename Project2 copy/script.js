ea1779e1dfdafcb9b44f6a9b6f115eecf20cf78a
joejiang111 <jyhjoe@gmail.com>
2022-08-09 12:21:54 -0400

    1

--- Project2 copy/script.js
+++ Project2 copy/script.js
@@ -42,6 +42,16 @@ function clean(chartType) {
     group2.selectAll('circle').transition().attr('opacity', 0);
     group2.selectAll('rect').transition().attr('opacity', 0);
   }
+  if (chartType !== 'group3') {
+    group3.selectAll('text').transition().attr('opacity', 0);
+    group3.selectAll('circle').transition().attr('opacity', 0);
+    group3.selectAll('path').transition().attr('opacity', 0);
+  }
+  if (chartType !== 'group4') {
+    group4.selectAll('text').transition().attr('opacity', 0);
+    group4.selectAll('circle').transition().attr('opacity', 0);
+    group4.selectAll('rect').transition().attr('opacity', 0);
+  }
 }
 
 function draw1() {
@@ -351,18 +361,251 @@ function draw3() {
 }
 
 function draw4() {
+  let y = d3.scaleBand().range([0, 400]).domain(d3.range(25));
+  let x = d3.scaleBand().range([0, 400]).domain(d3.range(25));
+
   clean('group3');
   group3
     .append('circle')
     .attr('cx', 400)
     .attr('cy', 400)
+    .transition()
+    .duration(1000)
     .attr('r', 100)
     .attr('fill', '#f15a24');
+
+  d3.select('#explore1').on('click', function () {
+    drawCut(13, 5);
+  });
+  d3.select('#explore2').on('click', function () {
+    drawCredit(8, 5);
+  });
+  d3.select('#explore3').on('click', function () {
+    drawAgency(10, 5);
+  });
+
+  // function explore() {
+  //   drawCut();
+  //   drawCredit();
+  //   drawAgency();
+  // }
+
+  function drawCut(col, row) {
+    const curve = d3.line().curve(d3.curveBasis);
+    const points = [
+      [400, 400],
+      [400, 600],
+      [300, 600],
+      // [230, 540],
+    ];
+    path = group3
+      .append('path')
+      .attr('d', curve(points))
+      .attr('stroke', '#f15a24')
+      .attr('stroke-width', '10px')
+      .attr('fill', 'none')
+      .attr('opacity', 0);
+
+    path.transition().duration(3000).attr('opacity', 0.2);
+
+    let data = d3.range(col * row);
+
+    group3
+      .append('g')
+      .selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .transition()
+
+      .delay(function (d, i) {
+        return i * 20;
+      })
+      .duration(2500)
+      .attr('cx', function (d) {
+        return x(d % col) + 80;
+      })
+      .attr('cy', function (d) {
+        return y(Math.floor(d / col)) + 570;
+      })
+      .attr('r', 5)
+      .attr('fill', '#f15a24')
+      .attr('opacity', 1);
+
+    text1 = group3
+      .append('text')
+      .attr('x', 75)
+      .attr('y', 550)
+      .attr('opacity', 0)
+      .style('font-size', 40)
+      .style('font-family', 'Arial')
+      .attr('fill', '#f15a24')
+      .text('65%');
+
+    text1.transition().duration(5000).attr('opacity', 1);
+  }
+
+  function drawCredit(col, row) {
+    const curve = d3.line().curve(d3.curveBasis);
+    const points = [
+      [400, 400],
+      [400, 600],
+      [500, 600],
+      // [230, 540],
+    ];
+    path = group3
+      .append('path')
+      .attr('d', curve(points))
+      .attr('stroke', '#f15a24')
+      .attr('stroke-width', '10px')
+      .attr('fill', 'none')
+      .attr('opacity', 0);
+
+    path.transition().duration(3000).attr('opacity', 0.2);
+
+    let data = d3.range(col * row);
+
+    group3
+      .append('g')
+      .selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .transition()
+
+      .delay(function (d, i) {
+        return i * 20;
+      })
+      .duration(2500)
+      .attr('cx', function (d) {
+        return x(d % col) + 520;
+      })
+      .attr('cy', function (d) {
+        return y(Math.floor(d / col)) + 570;
+      })
+      .attr('r', 5)
+      .attr('fill', '#f15a24')
+      .attr('opacity', 1);
+
+    text1 = group3
+      .append('text')
+      .attr('x', 520)
+      .attr('y', 550)
+      .attr('opacity', 0)
+      .style('font-size', 40)
+      .style('font-family', 'Arial')
+      .attr('fill', '#f15a24')
+      .text('40%');
+
+    text1.transition().duration(5000).attr('opacity', 1);
+  }
+
+  function drawAgency(col, row) {
+    const curve = d3.line().curve(d3.curveBasis);
+    const points = [
+      [400, 400],
+      [400, 200],
+      [500, 200],
+      // [230, 540],
+    ];
+    path = group3
+      .append('path')
+      .attr('d', curve(points))
+      .attr('stroke', '#f15a24')
+      .attr('stroke-width', '10px')
+      .attr('fill', 'none')
+      .attr('opacity', 0);
+
+    path.transition().duration(3000).attr('opacity', 0.2);
+
+    let data = d3.range(col * row);
+
+    group3
+      .append('g')
+      .selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .transition()
+
+      .delay(function (d, i) {
+        return i * 20;
+      })
+      .duration(2500)
+      .attr('cx', function (d) {
+        return x(d % col) + 520;
+      })
+      .attr('cy', function (d) {
+        return y(Math.floor(d / col)) + 170;
+      })
+      .attr('r', 5)
+      .attr('fill', '#f15a24')
+      .attr('opacity', 1);
+
+    text1 = group3
+      .append('text')
+      .attr('x', 520)
+      .attr('y', 150)
+      .attr('opacity', 0)
+      .style('font-size', 40)
+      .style('font-family', 'Arial')
+      .attr('fill', '#f15a24')
+      .text('52%');
+
+    text1.transition().duration(5000).attr('opacity', 1);
+  }
 }
 
-function draw5() {}
+function draw5() {
+  clean('group4');
+  let y = d3.scaleBand().range([0, 500]).domain(d3.range(25));
+  let x = d3.scaleBand().range([0, 500]).domain(d3.range(25));
 
-let activationFunctions = [draw1, draw2, draw3, draw4];
+  function drawFPL1(col, row) {
+    let data = d3.range(col * row);
+    group4
+      .append('text')
+      .attr('x', 510)
+      .attr('y', 210)
+      .style('font-size', 30)
+      .style('font-family', 'Arial')
+      .attr('fill', '#f15a24')
+      .text('>400%');
+
+    group4
+      .append('rect')
+      .attr('x', 490)
+      .attr('y', 170)
+      .attr('height', 60)
+      .attr('width', 5)
+      .attr('fill', '#f15a24');
+
+    group4
+      .append('g')
+      .selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .transition()
+
+      .delay(function (d, i) {
+        return i * 20;
+      })
+      .duration(200)
+      .attr('cx', function (d) {
+        return x(d % col) + 450;
+      })
+      .attr('cy', function (d) {
+        return y(Math.floor(d / col)) + 180;
+      })
+      .attr('r', 8)
+      .attr('fill', '#f15a24')
+      .attr('opacity', 1);
+  }
+  drawFPL1(2, 3);
+}
+
+let activationFunctions = [draw1, draw2, draw3, draw4, draw5];
 
 //All the scrolling function
 //Will draw a new graph based on the index provided by the scroll
@@ -394,32 +637,3 @@ scroll.on('progress', function (index, progress) {
   if ((index == 2) & (progress > 0.7)) {
   }
 });
-
-// function cleanViz() {
-//   svg.selectAll('g').remove();
-// }
-function clickFPL() {
-  svg
-    .selectAll('circle')
-    .data(data)
-    .transition()
-    .delay(function (d, i) {
-      return i * 20;
-    })
-    .duration(1000)
-    // .attr('r', 8)
-    .attr('stroke-width', 4)
-    .attr('stroke', function (d) {
-      if (d <= 12) {
-        return green1;
-      } else if (13 <= d && d <= 37) {
-        return green2;
-      } else if (38 <= d && d <= 60) {
-        return green3;
-      } else {
-        return 'grey';
-      }
-    });
-}
-
-function getStroke() {}
